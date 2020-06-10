aea112b590f86c280edb71eef00153ac5f893469
houfengtai <houfengtai@sumawuliu.com>
2020-06-10 23:13:16 +0800

    init pro

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,6 @@ export default {
   },
   methods: {
     _onresize () {
-      // const that = this
       window.onresize = () => {
         return (() => {
           const screenWidth = document.body.clientWidth
@@ -38,14 +37,14 @@ export default {
       this._dealAndroidInputMethodProblem()
     },
     /**
-       * 处理安卓手机点击输入框的时候，底部导航被输入法顶上来的问题
-       */
+     * 处理安卓手机点击输入框的时候，底部导航被输入法顶上来的问题
+     */
     _dealAndroidInputMethodProblem () {
       const userAgent = navigator.userAgent
       if (userAgent.indexOf('Android') > -1 || userAgent.indexOf('Linux') > -1) {
         /**
-           * 给所有非只读的输入框通过绑定获取焦点事件，用来把选中的输入框展示在屏幕中间位置
-           */
+         * 给所有非只读的输入框通过绑定获取焦点事件，用来把选中的输入框展示在屏幕中间位置
+         */
         this.$el.querySelectorAll('input:not([readonly])').forEach(
           e => e.addEventListener('focus', () => {
             this.scrollIntoView(true)
@@ -54,8 +53,8 @@ export default {
       }
     },
     /**
-       * 通过onresize事件，安卓系统弹出输入法的时候隐藏下边导航栏，收起时展示
-       */
+     * 通过onresize事件，安卓系统弹出输入法的时候隐藏下边导航栏，收起时展示
+     */
     _onresizeAndroidScreen () {
       const userAgent = navigator.userAgent
       if (userAgent.indexOf('Android') > -1 || userAgent.indexOf('Linux') > -1) {
@@ -75,15 +74,15 @@ export default {
       }
     },
     /**
-       * 给所有axios请求增加
-       * request拦截器：为了给请求header里添加token信息
-       * response拦截器：为了统一处理服务器响应和异常
-       * @private
-       */
+     * 给所有axios请求增加
+     * request拦截器：为了给请求header里添加token信息
+     * response拦截器：为了统一处理服务器响应和异常
+     * @private
+     */
     _addInterceptors () {
       /**
-         * 添加请求拦截器,统一为请求添加token信息
-         */
+       * 添加请求拦截器,统一为请求添加token信息
+       */
       axios.interceptors.request.use(
         config => {
           const authToken = window.localStorage.getItem('authToken')
@@ -96,8 +95,8 @@ export default {
       )
 
       /**
-         * 添加响应拦截器，统一处理服务器响应和异常
-         */
+       * 添加响应拦截器，统一处理服务器响应和异常
+       */
       axios.interceptors.response.use(
         response => {
           const { code, message } = response.data
@@ -119,15 +118,15 @@ export default {
         },
         error => {
           /**
-                   * 状态码401代表无权限访问，权限失效，需要重新获取authToken或openToken，此时授权成功后的默认跳转路由改为主页
-                   * 状态码500代表REST服务器异常
-                   */
+           * 状态码401代表无权限访问，权限失效，需要重新获取authToken或openToken，此时授权成功后的默认跳转路由改为主页
+           * 状态码500代表REST服务器异常
+           */
           const status = error.response.status
           const message = error.response.data.message
           if (status === 401 && this.$route.name !== 'lottery') {
             /**
-                     * 登录授权token超时，提示
-                     */
+             * 登录授权token超时，提示
+             */
             window.localStorage.removeItem('authToken')
             window.localStorage.removeItem('userInfo')
             this.$router.push({ name: 'login', params: { redirectPath: this.$route.fullPath } })
@@ -148,8 +147,8 @@ export default {
   },
   mounted () {
     /**
-       * 处理安卓手机点击输入框的时候，底部导航被输入法顶上来的问题
-       */
+     * 处理安卓手机点击输入框的时候，底部导航被输入法顶上来的问题
+     */
     this._dealAndroidInputMethodProblem()
     this._onresizeAndroidScreen()
     this._onresize()
@@ -158,43 +157,16 @@ export default {
 </script>
 
 <style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    color: #2c3e50;
-    margin: 0px;
-    padding:0px;
-    font-size:14px;
-  }
-  body{
-    margin: 0px;
-    padding:0px;
-    background: #ecf0f1;
-  }
-  a{
-    text-decoration: none;
-    color: #2c3e50;
-  }
-  a:hover{
-    text-decoration: none;
-    color: #00c1dc;
-  }
+  #app {font-family: 'Avenir', Helvetica, Arial, sans-serif;-webkit-font-smoothing: antialiased;-moz-osx-font-smoothing: grayscale;color: #2c3e50;margin: 0px; padding:0px;font-size:14px;}
+  body{margin: 0px;padding:0px;background: #ecf0f1;}
+  a{text-decoration: none;color: #2c3e50;}
+  a:hover{text-decoration: none;color: #00c1dc;}
   .clear{clear: both;}
   .border-top{border-top:1px solid #e5e5e5;}
   .border-bottom{border-bottom:1px solid #e5e5e5;}
   .border-left{border-left:1px solid #e5e5e5;}
   .border-right{border-right:1px solid #e5e5e5;}
-  .bg-dialog{
-    position: fixed;
-    top:0;
-    left:0;
-    background: #000;
-    opacity: 0.5;
-    width: 100%;
-    height: 100%;
-    z-index: 1;
-  }
+  .bg-dialog{position: fixed;top:0;left:0;background: #000;opacity: 0.5;width: 100%;height: 100%;z-index: 1;}
   .float-left{float:left;}
   .float-right{float:right;}
   .index-box{max-width:800px;margin:auto;padding:10px 15px;background: #fff;}
